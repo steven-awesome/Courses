2a5d6ef7b1ad77566f6eaab0bb9251b195eb07aa
steven <s.morrissey86@gmail.com>
2018-05-02 23:56:51 -0400

    Finish JWT

--- node-todo-api/server/server.js
+++ node-todo-api/server/server.js
@@ -97,6 +97,19 @@ app.post('/users', (req, res) => {
     });
 });
 
+app.get('/users/me', (req, res) => {
+    var token = req.header('x-auth');
+
+    User.findByToken(token).then((user) => {
+        if (!user) {
+            return Promise.reject();
+        }
+        res.send(user);
+    }).catch((e) => {
+        res.status(401).send();
+    });
+});
+
 app.listen(port, () => {
     console.log(`Started on port ${port}`);
 });
